07623a888805c716d114ebf5e85fd7d6a72ad3d6
Mihail Mihaylov <liahim13@gmail.com>
2026-01-25 17:01:55 +0200

    Added a setenv.env file with env variables, updated _docker.bat file

--- etls/datastore/financial_data_1_ethereum/docker/run_financial_data_1_ethereum_docker.bat
+++ etls/datastore/financial_data_1_ethereum/docker/run_financial_data_1_ethereum_docker.bat
@@ -33,6 +33,16 @@ for %%I in ("%cd%\..") do set "SCRIPT_NAME=%%~nI"
 
 set "CURRENT_DIR=%cd%"
 
+:: Store the path to the config file in a variable and load the external environment config.
+:: Going up 4 levels to find config: script_runner -> project -> datastore -> etls -> datawarehouse
+set "ENV_PATH=%~dp0..\..\..\..\config\local\setenv.env"
+
+::  Verify that the file exists
+if not exist "%ENV_PATH%" (
+    echo [ERROR] Environment file not found at: %ENV_PATH%
+    exit /b 1
+    )
+
 :: 6. Print a unified header
 echo ============================================================
 echo RUNNING SCRIPT:  %~n0
@@ -70,7 +80,8 @@ echo.
 :: Inside the container, treat localhost as the host machine.
 :: It allows the container to access services running on Windows.
 echo Running the container...
-docker run --rm --add-host=localhost:host-gateway %SCRIPT_NAME% %*
+docker run --rm --env-file "%ENV_PATH%" --add-host=localhost:host-gateway %SCRIPT_NAME% %*
+::docker run --rm --add-host=localhost:host-gateway %SCRIPT_NAME% %*
 :: docker run --add-host=localhost:host-gateway %SCRIPT_NAME% "%~1" "%~2=%~3"
 
 :: 9. Capture exit code after Docker execution
